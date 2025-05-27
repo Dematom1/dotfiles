f4b98a793b6d5f04d620d04fe40101b8da7f0033
dematom1 <laszlo@hey.com>
2025-05-27 17:42:08 -0600

    maint: increase wezterm font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Monokai (dark) (terminal.sexy)"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 21
 
 config.enable_tab_bar = false
 
