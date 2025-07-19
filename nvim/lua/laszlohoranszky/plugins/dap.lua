da0cd0bb9ea193e5a9196680d929008fc12b6af3
dematom1 <laszlo@hey.com>
2025-07-18 22:01:41 -0600

    feat: add debugger

--- nvim/lua/laszlohoranszky/plugins/dap.lua
+++ nvim/lua/laszlohoranszky/plugins/dap.lua
@@ -1,56 +1,79 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"leoluz/nvim-dap-go",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			"nvim-neotest/nvim-nio",
-			"williamboman/mason.nvim",
-		},
-		opts = {
-			ensure_installed = { "cppdbg", "node2", "python" },
-			automatic_setup = true,
-		},
-		config = function()
-			local dap = require("dap")
-			local ui = require("dapui")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "mfussenegger/nvim-dap-python",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "jay-babu/mason-nvim-dap.nvim"
+    },
+    opts = {
+      ensure_installed = { "cppdbg", "node2", "python" },
+      automatic_setup = true,
+    },
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
 
-			require("dapui").setup()
-			require("dap-go").setup()
+      require("dap-python").setup("python3")
 
-			local map = vim.keymap.set
-			map("n", "<space>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
-			map("n", "<space>dC", dap.run_to_cursor, { desc = "DAP: Run to Cursor" })
-			map("n", "<space>?", function()
-				ui.eval(nil, { enter = true })
-			end, { desc = "DAP: Evaluate Variable" })
+      require("dapui").setup({
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      })
+      require("nvim-dap-virtual-text").setup({
+        commented = true, -- Show virtual text alongside comment
+      })
+      require("dap-go").setup({})
 
-			map("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
-			map("n", "<leader>df", dap.step_into, { desc = "DAP: Step Into" })
-			map("n", "<leader>dd", dap.step_over, { desc = "DAP: Step Over" })
-			map("n", "<leader>dg", dap.step_out, { desc = "DAP: Step Out" })
-			map("n", "<leader>dB", dap.step_back, { desc = "DAP: Step Back" })
-			map("n", "<leader>dr", dap.restart, { desc = "DAP: Restart" })
-			map("n", "<leader>dT", dap.terminate, { desc = "DAP: Terminate" })
-			map("n", "<leader>du", require("dapui").toggle, { desc = "DAP: Toggle UI" })
+      local map = vim.keymap.set
+      map("n", "<space>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+      map("n", "<space>dC", dap.run_to_cursor, { desc = "DAP: Run to Cursor" })
+      map("n", "<space>?", function()
+        ui.eval(nil, { enter = true })
+      end, { desc = "DAP: Evaluate Variable" })
 
-			-- Optional: open REPL
-			map("n", "<leader>dp", dap.repl.open, { desc = "DAP: Open REPL" })
+      map("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
+      map("n", "<leader>df", dap.step_into, { desc = "DAP: Step Into" })
+      map("n", "<leader>dd", dap.step_over, { desc = "DAP: Step Over" })
+      map("n", "<leader>dg", dap.step_out, { desc = "DAP: Step Out" })
+      map("n", "<leader>dB", dap.step_back, { desc = "DAP: Step Back" })
+      map("n", "<leader>dr", dap.restart, { desc = "DAP: Restart" })
+      map("n", "<leader>dT", dap.terminate, { desc = "DAP: Terminate" })
+      map("n", "<leader>dD", dap.disconnect, { desc = "DAP: Disconnect" })
+      map("n", "<leader>du", require("dapui").toggle, { desc = "DAP: Toggle UI" })
 
-			-- Auto-open/close UI on debug session
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
-			end
-		end,
-	},
+      -- Optional: open REPL
+      map("n", "<leader>dp", dap.repl.open, { desc = "DAP: Open REPL" })
+
+      -- Auto-open/close UI on debug session
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }
