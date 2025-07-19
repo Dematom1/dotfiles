f37474ee0214ec024729c1e0c5922ef12e4e32ce
dematom1 <laszlo@hey.com>
2025-07-19 00:00:48 -0600

    feat: add dadbod

--- nvim/lua/laszlohoranszky/plugins/dadbod.lua
+++ nvim/lua/laszlohoranszky/plugins/dadbod.lua
@@ -1,6 +1,37 @@
--- return {
--- 	"tpope/vim-dadbod",
--- 	"kristijanhusak/vim-dadbod-completion",
--- 	"kristijanhusak/vim-dadbod-ui",
--- }
-return {}
+return {
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+      "kristijanhusak/vim-dadbod-ui",
+      "hrsh7th/nvim-cmp",
+    },
+    lazy = true,
+    cmd = { "DBUI", "DB" },
+    ft = { "sql", "mysql", "psql", "pgsql", "sqlite" },
+    -- Add keys to ensure keymaps are available immediately
+    keys = {
+      { "<leader>sq", "<cmd>DBUI<CR>",                  desc = "Dadbod: Toggle UI" },
+      { "<leader>sc", "<cmd>DBUIToggleConnections<CR>", desc = "Dadbod: Toggle Connections Panel" },
+      { "<leader>sr", "<cmd>DBUIRenameConnection<CR>",  desc = "Dadbod: Edit Connection" },
+    },
+    config = function()
+      -- Your existing config without the keymaps
+      vim.g.db_driver_postgres = 'psql'
+      vim.g.db_driver_mysql = 'mysql'
+      vim.g.db_driver_sqlite = 'sqlite3'
+
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_win_size = 30
+      vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui_connections.json"
+
+      -- Keep only the autocmd
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dbui",
+        callback = function()
+          vim.keymap.set("n", "q", "<cmd>DBUIQuit<CR>", { buffer = true, desc = "Dadbod: Quit DB UI" })
+        end,
+      })
+    end,
+  },
+}
--- nvim/lua/laszlohoranszky/plugins/nvim-cmp.lua
+++ nvim/lua/laszlohoranszky/plugins/nvim-cmp.lua
@@ -1,78 +1,93 @@
 return {
-	"hrsh7th/nvim-cmp",
-	name = "cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"hrsh7th/cmp-nvim-lsp", -- This is generally recommended, although nvim_lsp source covers it. Good to explicitly have.
-		"hrsh7th/cmp-nvim-lua", -- <<-- NEW: For Neovim Lua API completion
-		"hrsh7th/cmp-nvim-lsp-signature-help", -- <<-- NEW: For LSP signature help
-		"hrsh7th/cmp-calc",
-		"hrsh7th/cmp-vsnip",
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			build = "make install_jsregexp",
-		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
+  "hrsh7th/nvim-cmp",
+  name = "cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",                  -- source for text in buffer
+    "hrsh7th/cmp-path",                    -- source for file system paths
+    "hrsh7th/cmp-nvim-lsp",                -- This is generally recommended, although nvim_lsp source covers it. Good to explicitly have.
+    "hrsh7th/cmp-nvim-lua",                -- <<-- NEW: For Neovim Lua API completion
+    "hrsh7th/cmp-nvim-lsp-signature-help", -- <<-- NEW: For LSP signature help
+    "hrsh7th/cmp-calc",
+    "hrsh7th/cmp-vsnip",
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip",     -- for autocompletion
+    "rafamadriz/friendly-snippets", -- useful snippets
+    "onsails/lspkind.nvim",         -- vs-code like pictograms
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- selects the previous item.
-				["<Tab>"] = cmp.mapping.select_next_item(), -- Tab also selects the next item.
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp", keyboard_length = 3 },
-				{ name = "luasnip" },
-				{ name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
-				{ name = "buffer", keyword_length = 2 },
-				{ name = "path", keyword_length = 2 },
-				{ name = "nvim_lua", keyword_length = 2 }, -- <<-- NEW: Enable nvim_lua source
-				{ name = "cmp_nvim_lsp_signature_help" }, -- <<-- NEW: Enable signature help source
-				{ name = "vsnip", keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
-				{ name = "calc" }, -- source for math calculation
-			}),
-			formatting = {
-				fields = { "menu", "abbr", "kind" },
-				format = function(entry, item)
-					local menu_icon = {
-						nvim_lsp = "λ",
-						vsnip = "⋗",
-						buffer = "Ω",
-						path = "🖫",
-						calc = "+",
-						nvim_lua = "∞",
-						cmp_nvim_lsp_signature_help = "✎",
-					}
-					item.menu = menu_icon[entry.source.name]
-					return item
-				end,
-			},
-		})
-	end,
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- selects the previous item.
+        ["<Tab>"] = cmp.mapping.select_next_item(),   -- Tab also selects the next item.
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp",                   keyword_length = 3 },
+        { name = "luasnip" },
+        { name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
+        { name = "buffer",                     keyword_length = 2 },
+        { name = "path",                       keyword_length = 2 },
+        { name = "nvim_lua",                   keyword_length = 2 }, -- <<-- NEW: Enable nvim_lua source
+        { name = "cmp_nvim_lsp_signature_help" },                    -- <<-- NEW: Enable signature help source
+        { name = "vsnip",                      keyword_length = 2 }, -- nvim-cmp source for vim-vsnip
+        { name = "calc" },                                           -- source for math calculation
+      }),
+      formatting = {
+        fields = { "menu", "abbr", "kind" },
+        format = function(entry, item)
+          local menu_icon = {
+            nvim_lsp = "λ",
+            vsnip = "⋗",
+            buffer = "Ω",
+            path = "🖫",
+            calc = "+",
+            nvim_lua = "∞",
+            cmp_nvim_lsp_signature_help = "✎",
+            dadbod = "🗄", -- dadbod
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+        end,
+      },
+    })
+    cmp.setup.filetype({ "sql", "mysql", "psql", "pgsql", "sqlite" }, {
+      sources = cmp.config.sources({
+        { name = "dadbod" },                                   -- SQL-specific completion from Dadbod
+        { name = "buffer",               keyword_length = 2 }, -- Still useful for words in the current SQL buffer
+        { name = "path" },                                     -- Path completion can sometimes be useful even in SQL
+        { name = "vim-dadbod-completion" },
+        -- You could also include 'nvim_lsp' here if you have an SQL LSP server setup (like sqls)
+        -- { name = "nvim_lsp" },
+      }),
+      -- You can override other settings here for SQL, e.g., mapping specific keys
+      -- mapping = cmp.mapping.preset.insert({
+      --   ["<C-j>"] = cmp.mapping.select_next_item(), -- Example: override key for SQL only
+      -- }),
+    })
+  end,
 }
